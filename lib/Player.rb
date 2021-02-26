e5c6e3ceb3d4a91a9bd8fa6f86a71c507616b883
Collins Tatang <makungongcollins@gmail.com>
2021-02-26 09:37:52 +0100

    The set player symbol created inside the Player class

--- lib/Player.rb
+++ lib/Player.rb
@@ -6,7 +6,18 @@ class Player
    def initialize()
 
    end
+
    def set_player_name(name)
     @@names.push(name)
    end
+   def set_player_symbol(sym)
+    if ['X', 'Y'].include?(sym)
+        sym == 'Y'?@@pl_1_sym = 'X' : @@pl_1_sym = 'Y'
+        @@pl_2_sym = 'Y' if @@pl_1_sym == 'X'
+        @@pl_2_sym = 'X' if @@pl_1_sym == 'Y'
+    end
+    end
+def get_player_symbol()
+    
+end
 end

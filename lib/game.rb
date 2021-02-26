9135fe681d3b9cae82b3b78f1131271aaae54330
tta2yta <tta2yta@gmail.com>
2021-02-26 13:13:37 +0300

    Added Check_full in game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
 class game
+    def initialize
+        @board=[[1,2,3],[4,5,6],[7,8,9]]
+    end
+
+    def check_full?
+        @board.all? do |item|
+    
+            item.all?{|val| %w[x y].include?(val)}
+        end
+    end
 end

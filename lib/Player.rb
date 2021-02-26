22cb20131d227758ec49dcb2d932e97605c9cc61
tta2yta <tta2yta@gmail.com>
2021-02-26 13:05:07 +0300

    Modified class instances

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,31 +1,27 @@
 class Player
-    @@names=[]
-    @@sym={"pl1"=>"","pl2"=>""}
-    @@pl_1_sym
+  def initialize
+    @names = []
+    @sym = { 'pl1' => '', 'pl2' => '' }
+end
 
-    def initialize()
+  def player_name(name)
+    @names.push(name)
+  end
 
+  def get_player_name(index)
+    @names[index]
+  end
+
+  def player_symbol?(sym)
+    if %w[X Y].include?(sym)
+      @sym['pl1'] = sym
+      @sym['pl2'] = sym == 'X' ? 'Y' : 'X'
+      return true
     end
+    false
+  end
 
-    def set_player_name(name)
-        @@names.push(name)
-    end
-
-    def get_player_name(index)
-        @@names[index]
-    end
-
-    def set_player_symbol?(sym)
-        if ['X', 'Y'].include?(sym)
-            @@sym["pl1"]=sym
-            sym=='X'?@@sym["pl2"]='Y' : @@sym["pl2"]='X'
-            return true
-        end
-       return false
-
-    end
-
-    def get_player_symbol(index)
-     @@sym[index]
-    end
+  def get_player_symbol(index)
+    @sym[index]
+  end
 end
